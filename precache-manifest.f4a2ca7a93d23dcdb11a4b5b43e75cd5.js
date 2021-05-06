3d17895cc671ef2b90c88e17054f5f91b8a2bbb4
samueldsr99 <samueldsr8@gmail.com>
2021-05-06 04:27:38 +0000

    Updates

---
+++ precache-manifest.f4a2ca7a93d23dcdb11a4b5b43e75cd5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d4fa83ffc5e027405974dd73a6066e9",
+    "url": "/landing-page/index.html"
+  },
+  {
+    "revision": "a46d1cdd9f01ae16c3aa",
+    "url": "/landing-page/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "1578d9d888132298a8c7",
+    "url": "/landing-page/static/js/2.6cddfee9.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/landing-page/static/js/2.6cddfee9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a46d1cdd9f01ae16c3aa",
+    "url": "/landing-page/static/js/main.12efb6a4.chunk.js"
+  },
+  {
+    "revision": "c47bf54d5a9892ae624b",
+    "url": "/landing-page/static/js/runtime-main.974e3ba7.js"
+  },
+  {
+    "revision": "1696ead234ee0c6a36a9a2559307590e",
+    "url": "/landing-page/static/media/profile.1696ead2.jpg"
+  }
+]);
